197bf902de677ad23b6f3e1725a96eaf8702970d
EmilyZhang123 <zhangbinjin_style@163.com>
2018-11-14 22:18:40 +0800

    angular first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:'<h1>xxx</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appName';
+  title = 'test';
+  name = 'zhangbinjin';
+  person = {
+    name:"Emily",
+    title:"h1"
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
